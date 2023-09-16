8bb6f249908215d7beb13052e88b9ecae6786947
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2023-09-16 11:05:32 +0900

    can use graphql

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import strawberry
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # (2) User型を定義する
 @strawberry.type
@@ -9,12 +11,14 @@ class User:
     name: str
     age: int
 
+
 # (3) Query(データの読み込み)を行うクラスを定義する
 @strawberry.type
 class Query:
     @strawberry.field
     def user(self) -> User:
         return User(name="Shota", age=22)
+
 
 # (4) スキーマを定義する
 schema = strawberry.Schema(query=Query)
@@ -29,8 +33,13 @@ app = FastAPI()
 # (7) /graphqlでGraphQL APIへアクセスできるようにし、適切なレスポンスを出力
 app.add_route("/graphql", graphql_app)
 
-
-app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 許可するオリジンを指定。安全な設定に注意してください。
+    allow_credentials=True,
+    allow_methods=["*"],  # 許可するHTTPメソッドを指定。必要に応じて調整してください。
+    allow_headers=["*"],  # 許可するHTTPヘッダーを指定。必要に応じて調整してください。
+)
 
 
 @app.get("/")
@@ -38,4 +47,6 @@ async def root():
     return {"message": "Hello World"}
 
 
-
+@app.get("/hello")
+async def root():
+    return {"message": "Hello"}
