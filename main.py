d2b4e84a79956495e5c9790347d7b199c55eb39b
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2023-09-16 15:31:02 +0900

    able to use spotify serch engine

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 # (1) 必要なライブラリをインポートする
+import os
+from pymongo import MongoClient
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
 import strawberry
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
 from fastapi.middleware.cors import CORSMiddleware
 
 
+
+load_dotenv()
+
+clint = MongoClient(os.environ['MONGO_URL'])
 # (2) User型を定義する
 @strawberry.type
 class User:
     name: str
     age: int
+    
+
+@strawberry.type
+class Song:
+    song_name : str
+    song_genre : str
+    
+
+@strawberry.type
+class Room:
+    user_id : int
+    name : str
+    
 
 
 # (3) Query(データの読み込み)を行うクラスを定義する
@@ -18,6 +40,15 @@ class Query:
     @strawberry.field
     def user(self) -> User:
         return User(name="Shota", age=22)
+    
+    @strawberry.field
+    def song(self) -> Song:
+        return Song(song_name='aaa', song_genre='bbbb')
+    
+    @strawberry.field
+    def room(self) -> Room:
+        return Room(user_id=1, name='部屋1')
+
 
 
 # (4) スキーマを定義する
@@ -50,3 +81,14 @@ async def root():
 @app.get("/hello")
 async def root():
     return {"message": "Hello"}
+
+client_id = '49b607371e524745b0200c82ae283fba'
+client_secret = 'f8100708fd044216bf3dfd9cd4854558'
+ 
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+ 
+keyword = 'よあそび'
+ 
+results = sp.search(q=keyword, limit=10, market="JP")
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx + 1, track['name'])
