c82b28f5ea303b68f17780208fbb1067bfca4dc9
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2023-09-17 20:42:35 +0900

    completed

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 from pymongo import MongoClient
+from typing import List
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
@@ -7,16 +8,67 @@ import strawberry
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
 from fastapi.middleware.cors import CORSMiddleware
+import random
+from collections import defaultdict
 
 
-client_id = '49b607371e524745b0200c82ae283fba'
-client_secret = 'f8100708fd044216bf3dfd9cd4854558'
- 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+load_dotenv()
 
-keyword = 'aa'
+client = MongoClient(os.environ["MONGO_URL"])
+db = client["RoomDB"]
 
 
-results = sp.search(q=keyword, limit=2, market="JP")
-for idx, track in enumerate(results['tracks']['items']):
-    print(idx + 1, track['name'], track)
+client_id = "49b607371e524745b0200c82ae283fba"
+client_secret = "f8100708fd044216bf3dfd9cd4854558"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(
+        client_id=client_id, client_secret=client_secret
+    )
+)
+
+
+# (2) User型を定義する
+@strawberry.type
+class User:
+    name: str
+    age: int
+
+
+@strawberry.type
+class Song:
+    song_name: str
+    category : List[str]
+
+
+@strawberry.type
+class Room:
+    room_id: int
+    user_id: List[int]
+    name: str
+    
+
+@strawberry.type
+class RegisterComplete:
+    user_id : int
+    category : List[str]
+
+
+@strawberry.input
+class CreateRoom:
+    user_id: int
+    room_name: str
+
+
+@strawberry.input
+class JoinRoom:
+    user_id: int
+    room_id: int
+    
+@strawberry.input
+class Register:
+    user_id : int
+    category : List[str]
+
+
+    
